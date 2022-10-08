70724b380c313825a20020a45bfd904eb323caf5
Or Cohen <or672001@gmail.com>
2022-10-08 20:40:08 +0300

    added reqs

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 mkdir /opt/minecraft-standby
 cp data/* /opt/minecraft-standby/
 cp minecraft-standby.service /usr/lib/systemd/system/
+pip3 install -r requirement.py
 systemctl daemon-reload
 systemctl enable minecraft-standby.service --now
