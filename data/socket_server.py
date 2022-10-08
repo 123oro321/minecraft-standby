854e81efe084872e1074fa64e28f2b85ba291ffa
Or Cohen <or672001@gmail.com>
2022-10-08 23:35:44 +0300

    Fixed typo

--- data/socket_server.py
+++ data/socket_server.py
@@ -90,36 +90,36 @@ class SocketServer:
                          ("(using ForgeModLoader) " if is_using_fml else "") + "(%s:%s).")
                         % (client_ip, addr[1], ip, port))
                     self.write_response(client_socket, json.dumps({"text": self.kick_message}))
-                    # with self.lock:
-                    #     if not self.starting:
-                    #         self.starting = True
-                    #         stack_response = requests.get("http://169.254.169.254/latest/meta-data/tags/instance/aws:cloudformation:stack-name")
-                    #         document_respone = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document")
-                    #         if stack_response.status_code == 200 and document_respone.status_code == 200:
-                    #             stack_name = stack_response.text
-                    #             document = document_respone.json()
-                    #             events = boto3.client('events', document["region"])
-                    #             response = events.put_events(
-                    #                 Entries=[
-                    #                     {
-                    #                         'DetailType': 'Standby join attempt',
-                    #                         'Source': f'mc.{stack_name}',
-                    #                         'Resources': [
-                    #                             f'arn:aws:ec2:{document["region"]}:{document["accountId"]}:instance/{document["instanceId"]}'
-                    #                         ],
-                    #                         'Detail': json.dumps({
-                    #                             'instance-id': document["instanceId"],
-                    #                             'client': client_ip
-                    #                         })
-                    #                     }
-                    #                 ]
-                    #             )
-                    #             self.logger.info(response)
-                    #             self.kick_message = "§bServer is already starting!\n§bPlease wait few minutes"
-                    #             self.version_text = "Starting"
-                    #             self.motd = "§4Sever in starting!\n§aPlease wait patiently"
-                    #         else:
-                    #             self.logger.error("Instance metadata could not be retrieved!")
+                    with self.lock:
+                        if not self.starting:
+                            self.starting = True
+                            stack_response = requests.get("http://169.254.169.254/latest/meta-data/tags/instance/aws:cloudformation:stack-name")
+                            document_response = requests.get("http://169.254.169.254/latest/dynamic/instance-identity/document")
+                            if stack_response.status_code == 200 and document_response.status_code == 200:
+                                stack_name = stack_response.text
+                                document = document_response.json()
+                                events = boto3.client('events', document["region"])
+                                response = events.put_events(
+                                    Entries=[
+                                        {
+                                            'DetailType': 'Standby join attempt',
+                                            'Source': f'mc.{stack_name}',
+                                            'Resources': [
+                                                f'arn:aws:ec2:{document["region"]}:{document["accountId"]}:instance/{document["instanceId"]}'
+                                            ],
+                                            'Detail': json.dumps({
+                                                'instance-id': document["instanceId"],
+                                                'client': client_ip
+                                            })
+                                        }
+                                    ]
+                                )
+                                self.logger.info(response)
+                                self.kick_message = "§bServer is already starting!\n§bPlease wait few minutes"
+                                self.version_text = "Starting"
+                                self.motd = "§4Sever in starting!\n§aPlease wait patiently"
+                            else:
+                                self.logger.error("Instance metadata could not be retrieved!")
                 else:
                     self.logger.info(
                         "[%s:%d] Tried to request a login/ping with an unknown state: %d" % (client_ip, addr[1], state))
