8140be4c1383ac2d1c807d667952981233e013f8
Or Cohen <or672001@gmail.com>
2022-10-08 21:38:32 +0300

    Added systemd logger

--- data/main.py
+++ data/main.py
@@ -7,6 +7,7 @@ import json
 import os.path
 
 from socket_server import SocketServer
+from systemd.journal import JournaldLogHandler
 
 server = None
 
@@ -18,9 +19,13 @@ def main():
     logger.setLevel(logging.INFO)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
+    journal_handler = JournaldLogHandler()
+    journal_handler.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     console_handler.setFormatter(formatter)
+    journal_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
+    logger.addHandler(journal_handler)
     if os.path.exists("config.json"):
         logger.info("Loading configuration...")
         with open("config.json", 'r', encoding='windows-1255') as file:
