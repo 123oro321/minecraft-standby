c95c90e1003c6ca071e3ffb5920b5f97a6cf6c22
Or Cohen <or672001@gmail.com>
2022-10-08 22:03:52 +0300

    The last stupid fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 mkdir /opt/minecraft-standby
 cp data/* /opt/minecraft-standby/
 cp minecraft-standby.service /usr/lib/systemd/system/
-yum install installpython3-devel gcc systemd-devel -y
+yum install python3-devel gcc systemd-devel -y
 pip3 install --upgrade pip
 pip3 install -r requirements.txt
 systemctl daemon-reload
