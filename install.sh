9f2c26896de1e04be9272c6fbe242524c213aac7
Or Cohen <or672001@gmail.com>
2022-10-08 21:19:08 +0300

    fixed stoopid stuff 2

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 mkdir /opt/minecraft-standby
 cp data/* /opt/minecraft-standby/
 cp minecraft-standby.service /usr/lib/systemd/system/
-pip3 install -r requirement.py
+pip3 install -r requirements.txt
 systemctl daemon-reload
 systemctl enable minecraft-standby.service --now
