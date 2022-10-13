97e25948bbf026b5b337e232edd3333aef7dbef1
Or Cohen <or672001@gmail.com>
2022-10-13 22:40:27 +0300

    Reformatted and removed redundancies

--- data/main.py
+++ data/main.py
@@ -9,13 +9,9 @@ import os.path
 from socket_server import SocketServer
 from systemd.journal import JournaldLogHandler
 
-server = None
-
 
 def main():
     logger = logging.getLogger("FakeMCServer")
-    if not os.path.exists("logs"):
-        os.makedirs("logs")
     logger.setLevel(logging.INFO)
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
@@ -26,40 +22,31 @@ def main():
     journal_handler.setFormatter(formatter)
     logger.addHandler(console_handler)
     logger.addHandler(journal_handler)
-    if os.path.exists("config.json"):
+    if os.path.exists("standby.json"):
         logger.info("Loading configuration...")
-        with open("config.json", 'r', encoding='windows-1255') as file:
+        with open("standby.json", 'r', encoding='windows-1255') as file:
             configuration = json.load(file,)
 
         ip = configuration["ip"]
         port = configuration["port"]
-        motd = configuration["motd"]["1"] + "\n" + configuration["motd"]["2"]
+        motd = configuration["motd"]["first_line"] + "\n" + configuration["motd"]["second_line"]
         version_text = configuration["version_text"]
-        kick_message = ""
         samples = configuration["samples"]
-        try:
-            show_hostname = configuration["show_hostname_if_available"]
-        except KeyError:
-            configuration["show_hostname_if_available"] = True
-            show_hostname = True
-            with open("config.json", 'w', encoding='windows-1255') as file:
-                json.dump(configuration, file, sort_keys=True, indent=4, ensure_ascii=False)
+        show_hostname = configuration["show_hostname_if_available"]
 
         player_max = configuration.get("player_max", 0)
         player_online = configuration.get("player_online", 0)
         protocol = configuration.get("protocol", 2)
-        server_icon = None
-
-        for message in configuration["kick_message"]:
-            kick_message += message + "\n"
+        kick_message = "\n".join(configuration["kick_message"])
 
         if not os.path.exists(configuration["server_icon"]):
             logger.warning("Server icon doesn't exists - submitting none...")
+            server_icon = None
         else:
             with open(configuration["server_icon"], 'rb') as image:
                 server_icon = "data:image/png;base64," + base64.b64encode(image.read()).decode()
+        server = None
         try:
-            global server
             logger.info("Setting up server...")
             server = SocketServer(ip, port, motd, version_text, kick_message, samples, server_icon, logger, show_hostname, player_max, player_online, protocol)
             server.start()
@@ -71,27 +58,28 @@ def main():
         except Exception as e:
             logger.exception(e)
     else:
-        logger.warning("No configuration file found. Creating config.json...")
+        logger.warning("No configuration file found. Creating standby.json...")
         configuration = {
             "ip": "0.0.0.0",
             "port": 25565,
             "protocol": 2,
             "motd": {
-                "1": "§4Maintenance!",
-                "2": "§aCheck example.com for more information!",
+                "first_line": "§4Maintenance!",
+                "second_line": "§aCheck example.com for more information!",
             },
             "version_text": "§4Maintenance",
             "kick_message": ["§bSorry", "", "§aThis server is offline!"],
             "server_icon": "server_icon.png",
             "samples": ["§bexample.com", "", "§4Maintenance"],
+            "show_hostname_if_available": True,
             "show_ip_if_hostname_available": True,
             "player_max": 0,
-            "player_online": 0
+            "player_online": 0,
         }
 
-        with open("config.json", 'w', encoding='windows-1255') as file:
+        with open("standby.json", 'w', encoding='windows-1255') as file:
             json.dump(configuration, file, sort_keys=True, indent=4, ensure_ascii=False)
-        logger.info("Please adjust the settings in the config.json!")
+        logger.info("Please adjust the settings in the standby.json!")
         exit(1)
 
 
